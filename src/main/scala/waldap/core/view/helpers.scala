61be35e0978407cf009814012b9e61f0f72b24ff
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2017-09-25 02:01:38 +0900

    Fix for context.path

--- src/main/scala/waldap/core/view/helpers.scala
+++ src/main/scala/waldap/core/view/helpers.scala
@@ -1,14 +1,15 @@
 package waldap.core.view
 
 import org.webjars.WebJarAssetLocator
+import waldap.core.controller.Context
 
 object helpers {
   val webJarsLocator: WebJarAssetLocator = new WebJarAssetLocator()
 
-  def webjars(webjar: String, exactPath: String): String = {
+  def webjars(webjar: String, exactPath: String)(implicit context: Context): String = {
     Option(webJarsLocator.getFullPathExact(webjar, exactPath)) match {
       case Some(fullPath) =>
-        fullPath.replaceFirst("^META-INF/resources", "")
+        fullPath.replaceFirst("^META-INF/resources", context.path)
       case None =>
         throw new IllegalArgumentException(s"WebJar resource not found: ${webjar}, ${exactPath}")
     }
